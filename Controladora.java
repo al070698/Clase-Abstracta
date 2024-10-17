7f3ba4f1d1dd5ba6c9837ce6f5f5c5aaddd03d9f
al070698 <145055756+al070698@users.noreply.github.com>
2024-10-17 12:48:12 -0600

    Add files via upload

---
+++ Controladora.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Controladora {
+   public static void main(String[] args) {
+        new Thread(new Reloj()).start();
+        new Thread(new Ejecutor("calc", 0)).start();
+}
+}
